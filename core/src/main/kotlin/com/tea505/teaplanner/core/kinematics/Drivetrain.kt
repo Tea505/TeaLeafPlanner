3533090e02657dc13a4383c5a9249cbbdb59283a
Muhammad Tauha <mtauha9@gmail.com>
2024-10-23 19:28:08 -0400

    Removed Localizer variable.

--- core/src/main/kotlin/com/tea505/teaplanner/core/kinematics/Drivetrain.kt
+++ core/src/main/kotlin/com/tea505/teaplanner/core/kinematics/Drivetrain.kt
@@ -1,12 +1,9 @@
 package com.tea505.teaplanner.core.kinematics
 
 import com.tea505.teaplanner.core.geometry.Pose
-import com.tea505.teaplanner.core.localization.Localizer
 
 // A Drivetrain interface
 interface Drivetrain {
-
-    var localizer: Localizer
 
     fun set(pose: Pose)
 }
